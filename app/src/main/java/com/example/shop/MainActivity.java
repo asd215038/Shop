7f8a98b1894219b56164546bf7dded95d0cb8339
asd215038 <132047105+asd215038@users.noreply.github.com>
2024-07-29 16:59:23 +0800

    完成按鈕功能與地圖按鈕介面設定

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.shop;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +24,29 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void onlineOrderFood(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://inline.app/order/-MZH-xZRTVVGkgxbWV95:inline-live-2/-MZMQWjHa2IQYgH9-V_I"));
+        startActivity(i);
+    }
+
+    public void mapLocation(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:24.155806166074015, 120.66155956819078?q=漢來海港餐廳 台中店"));
+        startActivity(i);
+    }
+
+    public void onlineBooking(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://inline.app/booking/-MZH-xZRTVVGkgxbWV95:inline-live-2/-MZMQWjHa2IQYgH9-V_I"));
+        startActivity(i);
+    }
+
+    public void clickCall(View view) {
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:+07-412-8068"));
+        startActivity(i);
+    }
+
 }
